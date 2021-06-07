b8b06a95138534ea55d8dfb7e84d6f0c3faea502
Anders Musikka <anders@andersmusikka.se>
2021-06-07 16:26:52 +0200

    Support for stable rustc min const generics

--- savefile/src/prelude.rs
+++ savefile/src/prelude.rs
@@ -1,7 +1,7 @@
 pub use {
-    introspect_item, load, load_encrypted_file, load_file, load_file_noschema, load_from_mem, load_noschema, save,
-    save_encrypted_file, save_file, save_file_noschema, save_noschema, save_to_mem, Canary1, CryptoReader,
-    CryptoWriter, Deserialize, Deserializer, Field, Introspect, IntrospectItem, IntrospectedElementKey,
-    IntrospectionResult, Introspector, IntrospectorNavCommand, Removed, ReprC, SavefileError, Schema, SchemaEnum,
-    SchemaPrimitive, SchemaStruct, Serialize, Serializer, Variant, WithSchema,
+    super::introspect_item, super::load, super::load_encrypted_file, super::load_file, super::load_file_noschema, super::load_from_mem, super::load_noschema, super::save,
+    super::save_encrypted_file, super::save_file, super::save_file_noschema, super::save_noschema, super::save_to_mem, super::Canary1, super::CryptoReader,
+    super::CryptoWriter, super::Deserialize, super::Deserializer, super::Field, super::Introspect, super::IntrospectItem, super::IntrospectedElementKey,
+    super::IntrospectionResult, super::Introspector, super::IntrospectorNavCommand, super::Removed, super::ReprC, super::SavefileError, super::Schema, super::SchemaEnum,
+    super::SchemaPrimitive, super::SchemaStruct, super::Serialize, super::Serializer, super::Variant, super::WithSchema,
 };
