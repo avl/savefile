cd7361341cfde1fcf154ccbf028045f144b4f1e2
Anders Musikka <anders@andersmusikka.se>
2023-07-14 22:06:26 +0200

    align

--- savefile-test/src/test_nested_non_repr_c.rs
+++ savefile-test/src/test_nested_non_repr_c.rs
@@ -1,5 +1,13 @@
 use ::savefile::prelude::*;
 
+#[derive(Clone, Copy, Debug, PartialEq, Savefile)]
+#[savefile_unsafe_and_fast]
+struct CorrectlyAligned { //Used as a known-good to compare to
+    x: u32,
+    y: u32,
+}
+
+
 
 #[derive(Clone, Copy, Debug, PartialEq, Savefile)]
 #[savefile_unsafe_and_fast]
@@ -12,16 +20,26 @@ struct Inner {
 
 #[cfg(feature="nightly")] //The whole system to use a faster serialization/deserialization for Vec<T> where T:ReprC only works on nightly (since it depends on specialisation)
 #[test]
-#[should_panic] //Inner struct is not packed (same in memory as on disk)
 #[cfg(debug_assertions)] //This test only works in debug builds
-fn test_not_raw_memcpy1() {
-    use std::io::Cursor;
-	let sample  = vec![	
-        Inner { misaligner:0, x: 32}
-	];
+fn test_misaligned1() {
 
-    let mut f = Cursor::new(Vec::new());
-    {
-        Serializer::save_noschema(&mut f, 0, &sample).unwrap(); //Should panic here, Inner contains padding.
-    }
+    assert_eq!(unsafe { Inner::repr_c_optimization_safe(0).is_yes() }, false);
+    assert_eq!(unsafe { CorrectlyAligned::repr_c_optimization_safe(0).is_yes() }, true);
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Savefile)]
+#[savefile_unsafe_and_fast]
+struct Inner2 {
+    x: u32,
+    misaligner : u8,
+}
+
+
+#[cfg(feature="nightly")] //The whole system to use a faster serialization/deserialization for Vec<T> where T:ReprC only works on nightly (since it depends on specialisation)
+#[test]
+#[cfg(debug_assertions)] //This test only works in debug builds
+fn test_misaligned2() {
+
+    assert_eq!(unsafe { Inner2::repr_c_optimization_safe(0).is_yes() }, false);
+    assert_eq!(unsafe { CorrectlyAligned::repr_c_optimization_safe(0).is_yes() }, true);
 }
