d8407e9e92a640018cc54ca50c91c9090090fbae
Anders Musikka <anders@andersmusikka.se>
2024-04-28 21:41:13 +0200

    Compile tests

---
+++ compile_tests/tests/compile-fail/abi_ref_return.rs
@@ -0,0 +1,20 @@
+extern crate savefile;
+extern crate savefile_abi;
+extern crate savefile_derive;
+use std::collections::HashMap;
+use savefile::prelude::*;
+use savefile::{Deserialize, Deserializer, Serialize, Serializer};
+use std::fmt::Debug;
+use std::io::{BufWriter, Cursor, Write};
+use savefile_abi::AbiConnection;
+use savefile_derive::savefile_abi_exportable;
+
+#[savefile_abi_exportable(version = 0)]
+pub trait CallbackInterface {
+    fn set(&mut self, x: u32) -> u32;
+//~^ 14:34: 14:38: Method 'set': SavefileAbi does not support methods returning references.
+}
+
+fn main() {
+
+}
