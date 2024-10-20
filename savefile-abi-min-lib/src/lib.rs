7932214964483819ae74b1b46bf2e87e915e57c0
Anders Musikka <anders@andersmusikka.se>
2024-10-20 13:47:43 +0200

    wip

--- savefile-abi-min-lib/src/lib.rs
+++ savefile-abi-min-lib/src/lib.rs
@@ -1,17 +1,7 @@
 use savefile_derive::savefile_abi_exportable;
 use savefile_derive::Savefile;
 use std::fmt::{Debug, Formatter};
-use std::future::Future;
-use std::pin::Pin;
 
-
-/*
-#[async_trait::async_trait]
-#[savefile_abi_exportable(version = 0)]
-pub trait AdderCallback {
-    async fn set(&self, value: u32) -> u32;
-}
-*/
 
 #[derive(Savefile)]
 pub struct MyStuff {
@@ -31,17 +21,9 @@ pub trait AdderInterface {
     fn sub(&self, x: u32, y: u32, cb: Box<dyn AdderCallback>) -> u32;
     fn add_simple(&self, x: u32, y: u32) -> u32;
     fn do_nothing(&self);
-
-    fn async_add(&self, x: u32, y: u32) -> Pin<Box<dyn Future<Output = u32>>>;
 }
 impl Debug for dyn AdderInterface {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "AdderInterface")
     }
-}
-
-
-#[test]
-fn test(){
-
 }
